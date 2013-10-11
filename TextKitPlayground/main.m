ce101c644c7bdf2ea7f4233bab29d462481cd6a8
Julien Grimault <grimaultjulien@gmail.com>
2013-10-11 17:11:43 +0800

    Initial Commit

---
+++ TextKitPlayground/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TextKitPlayground
+//
+//  Created by Julien Grimault on 11/10/13.
+//  Copyright (c) 2013 XiaoGouSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XGSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XGSAppDelegate class]));
+    }
+}
---
+++ TextKitPlaygroundTests/TextKitPlaygroundTests.m
@@ -0,0 +1,34 @@
+//
+//  TextKitPlaygroundTests.m
+//  TextKitPlaygroundTests
+//
+//  Created by Julien Grimault on 11/10/13.
+//  Copyright (c) 2013 XiaoGouSoftware. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TextKitPlaygroundTests : XCTestCase
+
+@end
+
+@implementation TextKitPlaygroundTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
